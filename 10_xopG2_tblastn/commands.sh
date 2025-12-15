e87b8f53602513bd84f40a77854c786b0b945ce7
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-15 15:23:33 +0000

    A bit of a tidy up

--- 10_xopG2_tblastn/commands.sh
+++ 10_xopG2_tblastn/commands.sh
@@ -1,7 +1,7 @@
 ### Create directory containing genome sequences
-mkdir genomes
-cd genomes/
-ln -s ../../01_genomes/*.fasta .
+mkdir all_Xcc
+cd all_Xcc/
+ln -s ../../01_genomes_all_Xcc/*.fasta .
 
 ### Create BLAST databases
 for i in *.fasta; do echo $i; formatdb -pF -i $i ; done
@@ -13,8 +13,8 @@ for i in *.fasta; do echo $i; tblastn -seg no -query ../XopG2.faa -db $i -evalue
 cd ..
 
 ### Parse TBLASTN files
-./extract_flanking_regions.pl ./genomes/*.tblastn > xopG2_genomic_contexts_all-Xcc-tblastn.txt
-perl parse_blast_to_presence-absence_matrix.pl ./genomes/*.tblastn > gene_presence_absence_all-Xcc-tblastn.tsv
+./extract_flanking_regions.pl ./all_Xcc/*.tblastn > xopG2_genomic_contexts_all-Xcc-tblastn.txt
+perl parse_blast_to_presence-absence_matrix.pl ./all_Xcc/*.tblastn > gene_presence_absence_all-Xcc-tblastn.tsv
 
 
 
