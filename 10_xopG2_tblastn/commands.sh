7bd2b0d9fe85f5c257685717d17cff4bd37c44c2
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-15 15:33:50 +0000

    Ran analyses against the race-typed Xcc

--- 10_xopG2_tblastn/commands.sh
+++ 10_xopG2_tblastn/commands.sh
@@ -18,6 +18,28 @@ perl parse_blast_to_presence-absence_matrix.pl ./all_Xcc/*.tblastn > gene_presen
 
 
 
+02_genomes_race-typed
+
+
+### Create directory containing genome sequences
+mkdir race-typed_Xcc
+cd race-typed_Xcc/
+ln -s ../../02_genomes_race-typed/*.fasta .
+
+### Create BLAST databases
+for i in *.fasta; do echo $i; formatdb -pF -i $i ; done
+
+### Perform the TBLASTN searches
+for i in *.fasta; do echo $i; tblastn -seg no -query ../XopG2.faa -db $i -evalue 0.0000000001 -out XopG2.versus.$i.tblastn; done
+
+### Come back out of the new directory
+cd ..
+
+### Parse TBLASTN files
+./extract_flanking_regions.pl ./race-typed_Xcc/*.tblastn > xopG2_genomic_contexts_race-typed_Xcc_tblastn.txt
+perl parse_blast_to_presence-absence_matrix.pl ./race-typed_Xcc/*.tblastn > gene_presence_absence_race-typed_Xcc_tblastn.tsv
+
+
 
 
 
