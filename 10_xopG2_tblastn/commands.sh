6fd0763182af5b0538149bda3c40185d3259f13e
David Studholme <d.j.studholme@exeter.ac.uk>
2025-12-19 22:52:55 +0000

    Updated TBLASTN searches

--- 10_xopG2_tblastn/commands.sh
+++ 10_xopG2_tblastn/commands.sh
@@ -9,17 +9,12 @@ for i in *.fasta; do echo $i; formatdb -pF -i $i ; done
 ### Perform the TBLASTN searches
 for i in *.fasta; do echo $i; tblastn -seg no -query ../XopG2.faa -db $i -evalue 0.0000000001 -out XopG2.versus.$i.tblastn; done
 
-### Come back out of the new directory                                                                                                                                             
+### Come back out of the new directory
 cd ..
 
 ### Parse TBLASTN files
 ./extract_flanking_regions.pl ./all_Xcc/*.tblastn > xopG2_genomic_contexts_all-Xcc-tblastn.txt
 perl parse_blast_to_presence-absence_matrix.pl ./all_Xcc/*.tblastn > gene_presence_absence_all-Xcc-tblastn.tsv
-
-
-
-02_genomes_race-typed
-
 
 ### Create directory containing genome sequences
 mkdir race-typed_Xcc
@@ -38,10 +33,3 @@ cd ..
 ### Parse TBLASTN files
 ./extract_flanking_regions.pl ./race-typed_Xcc/*.tblastn > xopG2_genomic_contexts_race-typed_Xcc_tblastn.txt
 perl parse_blast_to_presence-absence_matrix.pl ./race-typed_Xcc/*.tblastn > gene_presence_absence_race-typed_Xcc_tblastn.tsv
-
-
-
-
-
-
-
